7fb9f9c5b818b3f3ccfdd0aac5580ad18906e4bf
Jelleas <jelle.van.assema@gmail.com>
2016-03-02 03:10:32 +0100

    Added main example

--- Main.java
+++ Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hi!");
+        System.out.println("Running examples of: ArrayList, LinkedList, BinaryHeap, BucketHashTable, HashSet");
+        System.out.println("======================================================");
+        ArrayList.main(args);
+        System.out.println("======================================================");
+        LinkedList.main(args);
+        System.out.println("======================================================");
+        BinaryHeap.main(args);
+        System.out.println("======================================================");
+        BucketHashTable.main(args);
+        System.out.println("======================================================");
+        HashSet.main(args);
+        System.out.println("======================================================");
     }
 }
