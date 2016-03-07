218893072e8b091861204c3a653c1e8f6778df02
Jelle van Assema <jelle.van.assema@gmail.com>
2016-03-07 18:44:48 +0100

    Updated main example

--- datastructures/Main.java
+++ datastructures/Main.java
@@ -3,7 +3,7 @@ package datastructures;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Running examples of: ArrayList, LinkedList, BinaryHeap, BucketHashTable, HashSet");
+        System.out.println("Running examples of: ArrayList, LinkedList, BinaryHeap, BucketHashTable, Trie, HashSet");
         System.out.println("======================================================");
         ArrayList.main(args);
         System.out.println("======================================================");
@@ -12,6 +12,8 @@ public class Main {
         BinaryHeap.main(args);
         System.out.println("======================================================");
         BucketHashTable.main(args);
+        System.out.println("======================================================");
+        Trie.main(args);
         System.out.println("======================================================");
         HashSet.main(args);
         System.out.println("======================================================");
